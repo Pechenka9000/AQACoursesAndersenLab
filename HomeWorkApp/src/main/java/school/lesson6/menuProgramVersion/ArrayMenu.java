06be14bddc5c8dcb8d855cc24ea068d260aeed06
pechenka9000 <pechenka9000@gmail.com>
2021-08-15 22:47:09 +0300

    Add "if" treatment for menuProgramVersion Author: Vladislav Gukov <pechenka9000@gmail.com> Date: Sun Aug 15 22:46:12 2021
    Add "if" blocks to ArrayMenu for treatment null variable values.
    
    Signed-off-by: Vladislav Gukov <pechenka9000@gmail.com>
    Signed-off-by: pechenka9000 <pechenka9000@gmail.com>

--- HomeWorkApp/src/main/java/school/lesson6/menuProgramVersion/ArrayMenu.java
+++ HomeWorkApp/src/main/java/school/lesson6/menuProgramVersion/ArrayMenu.java
@@ -24,49 +24,53 @@ public class ArrayMenu {
             }
             // Проверка корректности введённых данных(если введено "q" - завершение программы):
             dataChecker(sizeY);
-            ArrayCreator array = new ArrayCreator(Integer.parseInt(sizeX), Integer.parseInt(sizeY));
-            try {
-                array.setMyException();
-            } catch (MyArraySizeException e) {
-                e.printStackTrace();
-                break;
-            }
-            String[][] stringArray = array.getMyArray();
-            System.out.println("Введите НЕ число для создания ошибки: ");
-            String mistake = null;
-            try {
-                mistake = Utils.stringReader();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            System.out.println("Задайте координаты [i][j] для размещения ошибки в массиве: ");
+            if(sizeX != null & sizeY != null) {
+                ArrayCreator array = new ArrayCreator(Integer.parseInt(sizeX), Integer.parseInt(sizeY));
+                try {
+                    array.setMyException();
+                } catch (MyArraySizeException e) {
+                    e.printStackTrace();
+                    break;
+                }
+                String[][] stringArray = array.getMyArray();
+                System.out.println("Введите НЕ число для создания ошибки: ");
+                String mistake = null;
+                try {
+                    mistake = Utils.stringReader();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                System.out.println("Задайте координаты [i][j] для размещения ошибки в массиве: ");
                 System.out.println("Нажмите 'q' для выхода.");
-            String mistakeCoordinateX = null;
-            try {
-                mistakeCoordinateX = Utils.stringReader();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            // Проверка корректности введённых данных(если введено "q" - завершение программы):
+                String mistakeCoordinateX = null;
+                try {
+                    mistakeCoordinateX = Utils.stringReader();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                // Проверка корректности введённых данных(если введено "q" - завершение программы):
                 dataChecker(mistakeCoordinateX);
-            String mistakeCoordinateY = null;
-            try {
-                mistakeCoordinateY = Utils.stringReader();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            // Проверка корректности введённых данных(если введено "q" - завершение программы):
+                String mistakeCoordinateY = null;
+                try {
+                    mistakeCoordinateY = Utils.stringReader();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                // Проверка корректности введённых данных(если введено "q" - завершение программы):
                 dataChecker(mistakeCoordinateY);
-            try {
-                array.setMistake(stringArray, Integer.parseInt(mistakeCoordinateX)-1, Integer.parseInt(mistakeCoordinateY)-1, mistake);
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-            Utils.stringArrayOut2(stringArray);
-            try {
-                System.out.println(array.summer(stringArray));
-            } catch (MyArrayDataException e) {
-                e.printStackTrace();
+                try {
+                    if (mistakeCoordinateX != null & mistakeCoordinateY != null) {
+                        array.setMistake(stringArray, Integer.parseInt(mistakeCoordinateX) - 1, Integer.parseInt(mistakeCoordinateY) - 1, mistake);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                Utils.stringArrayOut2(stringArray);
+                try {
+                    System.out.println(array.summer(stringArray));
+                } catch (MyArrayDataException e) {
+                    e.printStackTrace();
+                }
             }
         }
     }
